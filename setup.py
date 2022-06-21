b77d09cb363ebf8784e4f40d14273c42080cceae
simondlevy <simon.d.levy@gmail.com>
2022-06-21 16:09:25 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+Python distutils setup file for gym-copter module.
+
+Copyright (C) 2022 Yosef Tamene, Simon D. Levy
+
+MIT License
+'''
+
+from setuptools import setup
+
+setup(
+    name='dynamixel_ax12',
+    version='0.1',
+    install_requires=['gym', 'numpy', 'Box2D', 'opencv-python'],
+    description='Gym environment for multicopters',
+    packages=['ax12', 'ax12.movement'],
+    author='Yosef Tamene, Simon D. Levy',
+    author_email='simon.d.levy@gmail.com',
+    url='https://github.com/simondlevy/dynamixel-ax12',
+    license='MIT',
+    platforms='Linux'
+    )
